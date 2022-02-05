8874c91c98b30b571a2d6222e7aea00fd492706c
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-02-05 11:58:20 +0900

    [Silver V] Title: 수 이어가기, Time: 100 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/2635.수 이어가기/수 이어가기.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+x = 1
+for a in range(1,n+1):
+    numbers = [n,a]
+    i =1
+
+    while True:
+        if numbers[i-1] < numbers[i]:
+            break
+        else:
+            numbers.append(numbers[i-1]-numbers[i])
+            i+=1
+
+    if x <= len(numbers):
+        x = len(numbers)
+        numbers_list = numbers
+
+print(x)
+for j in range(len(numbers_list)):
+    print(numbers_list[j], end=' ')
