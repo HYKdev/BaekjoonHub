becfab00d7063780ab26134a51f38b42eec6f704
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-02-21 21:36:27 +0900

    [Bronze I] Title: 색종이, Time: 1980 ms, Memory: 38476 KB -BaekjoonHub

---
+++ 백준/Bronze/10163.색종이/색종이.py
@@ -0,0 +1,13 @@
+arr = [[0 for _ in range(1001)] for _ in range(1001)]
+n = int(input())
+
+for k in range(1, n+1):
+    x, y, c, r = map(int, input().split())
+    for i in range(y, y+r):
+        arr[i][x:x+c] = [k]*c
+
+for j in range(1, n+1):
+    result = 0
+    for cnt in range(1001):
+        result += arr[cnt].count(j)
+    print(result)
