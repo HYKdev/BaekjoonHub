77f023d448c53d660f5051bf2c28bafc9fde07ed
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-03 03:03:52 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 988 ms, Memory: 73840 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -2,21 +2,14 @@ import sys
 
 n, p = map(int, sys.stdin.readline().split())
 
-lst = [[] for _ in range(n+1)]
+lst = [[0] for _ in range(n+1)]
 cnt = 0
 for _ in range(n):
     sound, fret = map(int, sys.stdin.readline().split())
-    if len(lst[sound]) == 0 or lst[sound][-1] < fret:
+    while lst[sound][-1] > fret:
+        lst[sound].pop()
         cnt += 1
+    if lst[sound][-1] != fret:
         lst[sound].append(fret)
-    elif lst[sound][-1] > fret:
-        while True:
-            if len(lst[sound]) == 0 or lst[sound][-1] < fret:
-                cnt += 1
-                lst[sound].append(fret)
-                break
-            if lst[sound][-1] == fret:
-                break
-            lst[sound].pop()
-            cnt += 1
+        cnt += 1
 print(cnt)
