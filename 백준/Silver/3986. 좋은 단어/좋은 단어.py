801fdca5a57b49a777c269f77c9c11f4d85ba8f7
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-03 00:34:51 +0900

    [Silver IV] Title: 좋은 단어, Time: 132 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+result = 0
+for _ in range(n):
+    arr = sys.stdin.readline().rstrip()
+    stack = []
+
+    for token in arr:
+        if stack:
+            if token == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(token)
+        else:
+            stack.append(token)
+    if not stack:
+        result += 1
+
+print(result)
