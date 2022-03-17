83caa093b3d81d5f10df24e74db7c7c326dfe0ae
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-17 21:41:43 +0900

    [Gold IV] Title: 치즈, Time: 120 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -5,42 +5,37 @@ dj = [1, -1, 0, 0]
 
 def BFS():
     q = deque()
+    visited[0][0] = 1
     q.append([0,0])
-    visited = [[0 for _ in range(c)] for _ in range(r)]
     cnt = 0
     while q:
-        i, j = q.popleft()
-        for k in range(4):
-            ni = i + di[k]
-            nj = j + dj[k]
-            if 0 <= ni < r and 0 <= nj < c:
-                if matrix[ni][nj] == 0 and not visited[ni][nj]:
-                    q.append([ni,nj])
-                    visited[ni][nj] = 1
+        cnt += 1
+        melt_cheese_list = []
+        while q:
+            i, j = q.pop()
+            for k in range(4):
+                ni = i + di[k]
+                nj = j + dj[k]
+                if 0 <= ni < r and 0 <= nj < c:
+                    if matrix[ni][nj] == 1 and visited[ni][nj] < 2:
+                        visited[ni][nj] += 1
+                        if visited[ni][nj] == 2:
+                            melt_cheese_list.append([ni,nj])
 
-                elif matrix[ni][nj] == 1:
-                    visited[ni][nj] += 1
+                    elif matrix[ni][nj] == 0 and visited[ni][nj] == 0:
+                        q.append([ni,nj])
+                        visited[ni][nj] = 1
+        
+        for i, j in melt_cheese_list:
+            matrix[i][j] = 0
+            visited[i][j] = 1
 
-    for i in range(r):
-        for j in range(c):
-            if visited[i][j] >= 2:
-                cnt += 1
-                matrix[i][j] = 0
-    
-    cheese_list.append(cnt)
-    return cnt
+        q = melt_cheese_list
+
+    return cnt-1
 # 입력
 r, c = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(r)]
+visited = [[0 for _ in range(c)] for _ in range(r)]
 
-
-cheese_list = []
-
-time = 0
-while True:
-    test_cnt = BFS()
-    time += 1
-    if test_cnt == 0:
-        break
-
-print(time-1)
+print(BFS())
