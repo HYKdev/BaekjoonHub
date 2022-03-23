3adaf8f169ff69b458cebc134b03555168e7a896
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-23 18:20:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4624 ms, Memory: 46368 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n = int(input())
+
+stack = []
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    stack.append([x, y])
+    
+stack.sort()
+
+for i, j in stack:
+    print(i, j)
