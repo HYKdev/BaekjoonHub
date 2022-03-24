f610a28257d8cdf27fb46fb74bf948eb1ca881e0
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-25 02:58:12 +0900

    [Bronze II] Title: 음계, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+ans = ''
+if arr == [1, 2, 3, 4, 5, 6, 7, 8]:
+    ans = 'ascending'
+elif arr == [8, 7, 6, 5, 4, 3, 2, 1]:
+    ans = 'descending'
+else:
+    ans = 'mixed'
+
+print(ans)
