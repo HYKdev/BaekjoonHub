850ec5434486d0f60c8658cf40aa6a5f67739c7a
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-03-27 19:57:59 +0900

    [Bronze II] Title: 블랙잭, Time: 152 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+ans = 0
+for lst in combinations(arr, 3):
+    if sum(lst) <= m:
+        ans = max(ans, sum(lst))
+
+print(ans)
