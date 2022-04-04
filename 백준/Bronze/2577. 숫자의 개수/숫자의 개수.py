f45dcdc0280cdb5294d0c320bfde8563de720f49
HYKdev <97649334+HYKdev@users.noreply.github.com>
2022-04-04 21:48:13 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 30872 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,17 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = A * B * C
+count_lst = [0 for _ in range(10)]
+
+while True:
+    count_lst[result % 10] += 1
+
+    if result // 10:
+        result = result//10
+    else:
+        break
+    
+for i in count_lst:
+    print(i)
